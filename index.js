ad0f572cb9714e1938ae630959e3c9553c0e2fef
Hassan Khan <153365025+Hassan-Khan-Developer@users.noreply.github.com>
2024-01-12 19:49:41 +0500

    wow

---
+++ index.js
@@ -0,0 +1,24 @@
+// ^ Navbar Script START 
+
+
+    
+  function myFunction(){
+      document.getElementById('navbar-mobile').classList.toggle("mystyle");
+  }
+
+            //   ^ Navbar Script END
+            // ^ TABS-CONTENT-Script START
+
+
+  function showContent(imageContentId, contentId) {
+    // Hide all content divs
+    document.querySelectorAll('.content').forEach(function (el) {
+      el.classList.add('hidden');
+    });
+
+    // Show the selected content
+    document.getElementById(imageContentId).classList.remove('hidden');
+    document.getElementById(contentId).classList.remove('hidden');
+  }
+           
+                //   ^ TABS-CONTENT-Script END 
